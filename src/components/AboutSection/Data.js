2241c24b51b13535dbbd21dfa386253b29b223e9
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-05-29 20:33:00 -0400

    added about elements

---
+++ src/components/AboutSection/Data.js
@@ -0,0 +1,62 @@
+// id => section
+// lightBg => background
+//lightText ==> heading
+// topLine => text about heading
+// imgstart: image first section
+
+// ABOUT SECTION
+export const homeObjOne = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Galit Gerasimov",
+  headline: "About me",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  buttonLabel:
+    "Currently available for full-time work and freelance opportunities!",
+  imgStart: false,
+  alt: "gg",
+  dark: false,
+  primary: true,
+  darkText: true,
+};
+
+// PORTFOLIO SECTION
+export const homeObjTwo = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Galit Gerasimov",
+  headline: "About me",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  buttonLabel:
+    "Currently available for full-time work and freelance opportunities!",
+  imgStart: false,
+  alt: "gg",
+  dark: false,
+  primary: true,
+  darkText: true,
+};
+
+// RESUME SECTION
+export const homeObjThree = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Galit Gerasimov",
+  headline: "About me",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  buttonLabel:
+    "Currently available for full-time work and freelance opportunities!",
+  imgStart: false,
+  alt: "gg",
+  dark: false,
+  primary: true,
+  darkText: true,
+};
