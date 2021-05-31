7187b5a7499ba3b45ca6df827256205399ce45a6
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-05-30 20:51:34 -0400

    added react scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import { animateScroll as scroll } from "react-scroll";
 import {
   Nav,
   NavbarContainer,
@@ -13,11 +14,30 @@ import {
 } from "./NavbarElements";
 
 export const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 720) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
   return (
     <>
-      <Nav>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
-          <NavLogo to="/">Galit Gerasimov</NavLogo>
+          <NavLogo to="/" onClick={toggleHome}>
+            Galit Gerasimov
+          </NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
@@ -26,10 +46,28 @@ export const Navbar = ({ toggle }) => {
               <NavLinks to="home">Home</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="about">About</NavLinks>
+              <NavLinks
+                to="about"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                About
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="portfolio">Portfolio</NavLinks>
+              <NavLinks
+                to="portfolio"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Portfolio
+              </NavLinks>
             </NavItem>
             <NavItem>
               <NavLinks to="resume">Resume</NavLinks>
