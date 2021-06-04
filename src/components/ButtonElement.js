2f96ed1ffb0001a520b88c12b811e9b2d297d31a
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-06-03 20:49:37 -0400

    updated buttom

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -2,11 +2,11 @@
 import styled from "styled-components";
 import { Link } from "react-scroll";
 
-export const Button = styled(Link)`
+export const Button = styled.a`
   border-radius: 50px;
-  background: ${({ primary }) => (primary ? "#01BF71" : "#010606")};
+  background: #f7ede2;
   white-space: nowrap;
-  padding: ${({ big }) => (big ? "14px 48px" : "12px 30px")};
+  padding: 5px 22px;
   color: ${({ dark }) => (dark ? "#010606" : "#fff")};
   font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
   outline: none;
@@ -16,9 +16,29 @@ export const Button = styled(Link)`
   justify-content: center;
   align-items: center;
   transition: all 0.2s ease-in-out;
+  text-decoration: none;
 
   &:hover {
+    color: #fff;
     transition: all 0.2s ease-in-out;
-    background: ${({ primary }) => (primary ? "#fff" : "#01BF71")};
+    background: ${({ primary }) => (primary ? "transparent" : "#01BF71")};
   }
 `;
+
+// border-radius: 50px;
+// background: #f7ede2;
+// white-space: nowrap;
+// padding: 5px 22px;
+// color: #010606;
+// font-size: 16px;
+// outline: none;
+// border: none;
+// cursor: pointer;
+// transition: all 0.2s ease-in-out;
+// text-decoration: none;
+
+// &:hover {
+//   transition: all 0.2s ease-in-out;
+//   background: #fff;
+//   color: #010606;
+// }
