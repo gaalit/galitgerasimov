6dfa75fc89d90b620f6164c00b1ea1f392f22485
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-06-03 20:53:24 -0400

    updated contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,45 @@
 import React from "react";
+import "./contact.css";
 
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className="contact-container">
+      <div className="wrapper-contact">
+        <div className="highlight"></div>
+        <h2 id="contact">Contact</h2>
+        <div className="contactInfoContainer">
+          <div className="formContainer">
+            <form name="contact" method="POST" data-netlify="true">
+              <label>
+                <input type="text" name="name" placeholder="Name" />
+              </label>
+              <label>
+                <input type="email" name="email" placeholder="Email" />
+              </label>
+              <label>
+                <textarea
+                  name="message"
+                  placeholder="Write a message here"
+                ></textarea>
+              </label>
+              <button type="submit">Send</button>
+            </form>
+          </div>
+          <div className="contactMeContainer">
+            <p>
+              You can contact me at{" "}
+              <a
+                className="contactEmail"
+                href="mailto:galit.gerasimov3@gmail.com"
+              >
+                galit.gerasimov3@gmail.com
+              </a>{" "}
+              or feel free to use the form provided and I'll get back to you as
+              soon as possible!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
