59f2d3a95e41d163989f103323bcb790a087448a
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-06-09 17:22:18 -0400

    added 2 additional buttons

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -1,13 +1,12 @@
 // button for react-scroll
 import styled from "styled-components";
-import { Link } from "react-scroll";
 
-export const Button = styled.a`
+export const ButtonOne = styled.a`
   border-radius: 50px;
   background: #f7ede2;
   white-space: nowrap;
   padding: 5px 22px;
-  color: ${({ dark }) => (dark ? "#010606" : "#fff")};
+  color: ${({ dark }) => (dark ? "#010606" : "#84a59d")};
   font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
   outline: none;
   border: none;
@@ -25,20 +24,36 @@ export const Button = styled.a`
   }
 `;
 
-// border-radius: 50px;
-// background: #f7ede2;
-// white-space: nowrap;
-// padding: 5px 22px;
-// color: #010606;
-// font-size: 16px;
-// outline: none;
-// border: none;
-// cursor: pointer;
-// transition: all 0.2s ease-in-out;
-// text-decoration: none;
+export const ButtonTwo = styled.a`
+  display: flex;
+  align-items: center;
+  background: hsla(0, 0%, 100%, 0.75);
+  border-radius: 7px;
+  color: #84a59d;
 
-// &:hover {
-//   transition: all 0.2s ease-in-out;
-//   background: #fff;
-//   color: #010606;
-// }
+  transition: all 0.2s ease-in-out;
+  text-decoration: none !important;
+
+  padding: 7px 22px;
+  box-shadow: inset 0 0 0 2px #333;
+`;
+
+export const ButtonThree = styled.a`
+  border-radius: 50px;
+  background: rgba(202, 174, 162, 0.6);
+  white-space: nowrap;
+  padding: 16px 64px;
+  color: #fff;
+  font-size: 1.2rem;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+
+    color: #010606;
+  }
+`;
