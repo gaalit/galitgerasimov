db1cb26bbfdfa1d28ab06a66e2266ee155be1362
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-06-09 17:24:02 -0400

    added content for portfolio section

---
+++ src/components/Portfolio/Data.js
@@ -0,0 +1,59 @@
+import madeLocal from "../../assets/images/madelocal.png";
+import jungle from "../../assets/images/jungle.gif";
+import scheduler from "../../assets/images/scheduler.gif";
+import tweeter from "../../assets/images/tweeter.gif";
+import tinyApp from "../../assets/images/tinyapp.gif";
+
+export const projectOneObj = {
+  titleOne: "Madelocal",
+  imgOne: madeLocal,
+  altOne: "screenshot of Made-Local app landing page",
+  descriptionOne:
+    "An app to connect users to local home-based and small businesses in their area. Created with React and Node.js",
+  hrefOne: "https://github.com/sabiat/made-local",
+};
+
+export const projectTwoObj = {
+  titleTwo: "MyWikiMaps",
+  imgTwo: madeLocal,
+  altTwo: "screenshot of MyWikiMaps app landing page",
+  descriptionTwo:
+    "A map based web app for creating lists of locations worth sharing. From restaurants to sights to activities, the lists are endless.",
+  hrefTwo: "https://github.com/rafreza/MyWikiMaps",
+};
+
+export const projectThreeObj = {
+  titleThree: "Jungle",
+  imgThree: jungle,
+  altThree: "screenshot of Jungle app landing page",
+  descriptionThree:
+    "An e-commerce style application built with MVC pattern using Ruby on Rails",
+  hrefThree: "https://github.com/gaalit/jungle-rails",
+};
+
+export const projectFourObj = {
+  titleFour: "Scheduler",
+  imgFour: scheduler,
+  altFour: "screenshot of Jungle app landing page",
+  descriptionFour:
+    "A React application that allows interviewees to book an appointment, choosing their favorite time slot, with an interviewer of their choosing.",
+  hrefFour: "https://github.com/gaalit/scheduler",
+};
+
+export const projectFiveObj = {
+  titleFive: "Tweeter",
+  imgFive: tweeter,
+  altFive: "screenshot of Tweeter app landing page",
+  descriptionFive:
+    "Tweeter is a simple, single-page Twitter clone. It is a student development project to touch on new technologies that are foreign to me. These technologies include, but not limited to, AJAX, CSS, SCSS, jQuery, HTML5 and CSS3.",
+  hrefFive: "https://github.com/gaalit/tweeter",
+};
+
+export const projectSixObj = {
+  titleSix: "TinyApp",
+  imgSix: tinyApp,
+  altSix: "screenshot of TinyApp app landing page",
+  descriptionSix:
+    "TinyApp is a full-stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly)",
+  hrefSix: "https://github.com/gaalit/tinyapp",
+};
