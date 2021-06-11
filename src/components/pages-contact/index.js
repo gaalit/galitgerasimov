9f57b2b2c9cee3842ef247d37f5017049c68f9ed
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-06-11 16:15:02 -0400

    set-up route for when contact message is successfully posted

---
+++ src/components/pages-contact/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactSuccess from "../ContactSuccess";
+import Footer from "../Footer";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <ContactSuccess />
+      <Footer />
+    </div>
+  );
+};
+
+export default ContactPage;
