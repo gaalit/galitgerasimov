4625117ffd82a8f4039204f6899b844156914cc9
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-06-11 16:11:48 -0400

    added route for when the message is successfully posted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages";
-import ContactSuccess from "./components/ContactSuccess";
+import ContactPage from "./components/pages-contact";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Home} exact></Route>
-        <Route path="/success=true" component={ContactSuccess} exact></Route>
+        <Route path="/success=true" component={ContactPage} exact></Route>
       </Switch>
     </Router>
   );
