702ae9f02776c26849d11c70f0fc02065472a10c
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-06-11 10:27:47 -0400

    added route for when contact form is successfully submited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages";
+import ContactSuccess from "./components/ContactSuccess";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Home} exact></Route>
+        <Route path="/success=true" component={ContactSuccess} exact></Route>
       </Switch>
     </Router>
   );
